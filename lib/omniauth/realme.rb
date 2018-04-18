026b846e0ff3415d371ccca9a86d20677592e5a2
DanHenton <Dan.henton@live.com>
2018-04-18 15:56:19 +1200

    init commit

---
+++ lib/omniauth/realme.rb
@@ -0,0 +1,7 @@
+require "omniauth/realme/version"
+
+module Omniauth
+  module Realme
+    # Your code goes here...
+  end
+end
---
+++ spec/omniauth/realme_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Omniauth::Realme do
+  it "has a version number" do
+    expect(Omniauth::Realme::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
