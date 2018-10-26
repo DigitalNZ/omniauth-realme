dad62780ee8738210804a9d2403d3ba284c7c9e0
DanHenton <Dan.henton@live.com>
2018-10-26 15:00:50 +1300

    add RealMe default error messages from omniauth errors

--- lib/omniauth/strategies/realme.rb
+++ lib/omniauth/strategies/realme.rb
@@ -23,7 +23,7 @@ module OmniAuth
         if response.is_valid?
           session[:uid] = response.nameid
         else
-          authorize_failure
+          session[:realme_error] = default_error_messages(response.errors.join)
         end
 
         @raw_info = response
@@ -42,11 +42,24 @@ module OmniAuth
           settings.authn_context                  = options.fetch('auth_strenght', 'urn:nzl:govt:ict:stds:authn:deployment:GLS:SAML:2.0:ac:classes:LowStrength')
           settings.protocol_binding                   = "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST"
           settings.assertion_consumer_service_binding = "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST"
-          settings.soft = false
+          settings.soft = true
 
           settings.security[:authn_requests_signed] = true
 
           settings
+      end
+
+      def default_error_messages(error)
+        case error
+        when /Timeout/
+          'Your RealMe session has expired due to inactivity.'
+        when /NoAvailableIDP/
+          'RealMe reported that the TXT service, Google Authenticator or the RealMe token service is not available. You may try again later. If the problem persists, please contact RealMe Help Desk on 0800 664 774.'
+        when /InternalError/
+          ' RealMe was unable to process your request due to a RealMe internal error. Please try again. If the problem persists, please contact RealMe Help Desk on 0800 664 774.'
+        else
+          "RealMe reported a serious application error with the message #{error}. Please try again later. If the problem persists, please contact RealMe Help Desk on 0800 664 774."
+        end
       end
     end
   end
