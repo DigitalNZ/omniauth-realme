cdd17611f4943734fc97ac955c3869fe058851c5
Eoin Kelly <eoin@eoinkelly.info>
2020-08-12 11:07:17 +1200

    Add method to generate SP metadata as an XML string (#14)
    * Add method to generate SP metadata as an XML string
    
    Realme MTS environment requires you to upload your SP Metadata before
    you can use MTS. This method (and associated README example) make it
    easy to generate the correct XML that exactly matches the settings you
    have used to configure this OmniAuth strategy.
    
    * Update specs to account for recent changes to master branch
    
    * Merging master accidentally made #allowed_clock_drift public
    
    It does not need to be public.

--- lib/omniauth/realme.rb
+++ lib/omniauth/realme.rb
@@ -3,9 +3,49 @@
 require 'omniauth/realme/version'
 require 'omniauth/strategies/realme'
 
-module Omniauth
-  module Realme
+module OmniAuth
+  module Realme # :nodoc:
     class Error < StandardError; end
-    # Your code goes here...
+
+    ##
+    # Generates SAML SP metadata XML using the same settings you used to
+    # configure the OmniAuth strategy. This XML is suitable for uploading to
+    # Realme at https://mts.realme.govt.nz/logon-mts/metadataupdate
+    #
+    # @param [Hash] options - An optional Hash of options to configure the
+    #                         strategy. This is convenient for testing but is
+    #                         not required during normal operation because the
+    #                         strategy will already be configured by the Rails
+    #                         initializer.
+    # @return [String] SP metadata serialized as an XML string
+    #
+    def self.generate_metadata_xml(options: {})
+      meta = OneLogin::RubySaml::Metadata.new
+
+      # OmniAuth strategies are Rack middlewares. When an instance of a rack
+      # middleware is created it is given a reference to the next rack
+      # app/middleware in the chain. We are only interested here in getting the
+      # SAML settings out of the strategy. We don't hit any code paths which
+      # would require a real rack app/middleware so `nil` works just fine.
+      rack_app = nil
+
+      # The Rails initializer calls `OmniAuth::Strategies::Realme.configure`
+      # which merges the provided block into the default options for
+      # `OmniAuth::Strategies::Realme` - use
+      # `OmniAuth::Strategies::Realme.default_options` to inspect the current
+      # state of these options.
+      #
+      # This means that the `options` we pass in here will be merged into (and
+      # override) those default options.
+      #
+      # When this method is called by app code, we want to use the options set
+      # by the Rails initializer so pass an empty hash as `options`. When this
+      # method is called by its specs, no Rails initializer has run so we need
+      # to pass in some options.
+      #
+      strategy = OmniAuth::Strategies::Realme.new(rack_app, options)
+
+      meta.generate(strategy.saml_settings)
+    end
   end
 end
--- spec/omniauth/realme_spec.rb
+++ spec/omniauth/realme_spec.rb
@@ -1,7 +1,73 @@
 # frozen_string_literal: true
 
-RSpec.describe Omniauth::Realme do
+RSpec.describe OmniAuth::Realme do
   it 'has a version number' do
-    expect(Omniauth::Realme::VERSION).not_to be nil
+    expect(OmniAuth::Realme::VERSION).not_to be nil
+  end
+
+  describe '.generate_metadata_xml' do
+    let(:idp_metadata_path) { File.join(__dir__, '../fixtures/realme_mts_idp_metadata.xml') }
+    let(:assertion_consumer_service_url) { 'http://www.example.com/auth/anything' }
+    let(:issuer) { 'Anything' }
+
+    let(:p12) { OpenSSL::PKCS12.new(File.read(File.join(__dir__, '../fixtures/mts_saml_sp.p12')), 'password') }
+    let(:sp_private_key) { p12.key.to_s }
+    let(:sp_public_key) { p12.certificate.to_s }
+    let(:sp_public_key_without_formatting) do
+      sp_public_key
+        .gsub!(/-----(BEGIN|END) CERTIFICATE-----/, '')
+        .gsub!("\n", '')
+    end
+
+    let(:realme_strategy_options) do
+      {
+        idp_service_metadata: idp_metadata_path,
+        issuer: issuer,
+        private_key: sp_private_key,
+        certificate: sp_public_key,
+        assertion_consumer_service_url: assertion_consumer_service_url,
+        allowed_clock_drift: 0
+      }
+    end
+
+    let(:expected_xml) do
+      <<~EO_XML
+        <?xml version="1.0" encoding="UTF-8"?>
+        <md:EntityDescriptor xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata" ID="PLACEHOLDER_ID" entityID="#{issuer}">
+          <md:SPSSODescriptor AuthnRequestsSigned="true" WantAssertionsSigned="true" protocolSupportEnumeration="urn:oasis:names:tc:SAML:2.0:protocol">
+            <md:KeyDescriptor use="signing">
+              <ds:KeyInfo xmlns:ds="http://www.w3.org/2000/09/xmldsig#">
+                <ds:X509Data>
+                  <ds:X509Certificate>#{sp_public_key_without_formatting}</ds:X509Certificate>
+                </ds:X509Data>
+              </ds:KeyInfo>
+            </md:KeyDescriptor>
+            <md:KeyDescriptor use="encryption">
+              <ds:KeyInfo xmlns:ds="http://www.w3.org/2000/09/xmldsig#">
+                <ds:X509Data>
+                  <ds:X509Certificate>#{sp_public_key_without_formatting}</ds:X509Certificate>
+                </ds:X509Data>
+              </ds:KeyInfo>
+            </md:KeyDescriptor>
+            <md:NameIDFormat>urn:oasis:names:tc:SAML:2.0:nameid-format:persistent</md:NameIDFormat>
+            <md:AssertionConsumerService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST" Location="#{assertion_consumer_service_url}" index="0" isDefault="true"/>
+          </md:SPSSODescriptor>
+        </md:EntityDescriptor>
+      EO_XML
+    end
+
+    let(:expected_xmldoc) { Nokogiri.XML(expected_xml) }
+
+    it 'generates the expected XML string' do
+      actual_xml = OmniAuth::Realme.generate_metadata_xml(options: realme_strategy_options)
+
+      actual_xmldoc = Nokogiri.XML(actual_xml)
+
+      # The ID is different every time so replace it with a known, predictable
+      # value before comparing with the expected output
+      actual_xmldoc.root.attributes['ID'].value = 'PLACEHOLDER_ID'
+
+      expect(actual_xmldoc.to_s).to eq(expected_xmldoc.to_s)
+    end
   end
 end
